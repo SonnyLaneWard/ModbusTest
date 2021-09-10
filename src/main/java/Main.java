f3ba863bbd7e816ed1a379cd0bbc599b5fa1adc4
SonnyLaneWard <ivan.semejkin@yandex.ru>
2021-09-10 16:42:12 +0300

    first

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import de.re.easymodbus.exceptions.ModbusException;
+import de.re.easymodbus.modbusclient.ModbusClient;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ModbusException {
+        ModbusClient modbusClient = new ModbusClient("192.168.0.218", 502);
+        modbusClient.Connect();
+
+        // Writing Single Coil
+
+        //modbusClient.WriteSingleCoil(1024,true); //Stop
+        //modbusClient.WriteSingleCoil(1024,false);
+        //modbusClient.WriteSingleCoil(1025,true); //Start
+        //modbusClient.WriteSingleCoil(1025,false);
+        //modbusClient.WriteSingleCoil(1026,true); //Alarm
+        //modbusClient.WriteSingleCoil(1026,false);
+
+        // Writing Holding Reg float
+        //modbusClient.WriteMultipleRegisters(1500,ModbusClient.ConvertFloatToTwoRegisters((float)(1323.55))); //sp T
+
+
+        // Read Input Status
+        //System.out.println(Arrays.toString(modbusClient.ReadDiscreteInputs(1024, 1))); // Статус работы
+
+
+
+
+
+
+        //Read Float Value from Input Reg
+       //System.out.println(ModbusClient.ConvertRegistersToFloat(modbusClient.ReadInputRegisters(1572, 2))); // ТОС
+
+
+
+    }
+}
